022fc662f8092f1984892cbc6067ef5968242482
MadishettiPrashanth <prashanthmadishetti1999@gmail.com>
2023-06-06 12:08:02 +0530

    Hello

---
+++ src/Git.java
@@ -0,0 +1,14 @@
+
+public class Git {
+	
+	public void m1() {
+		
+		System.out.println("Githubpractice");
+	}
+
+	public static void main(String[] args) {
+     Git m =new Git();
+     m.m1();
+	}
+
+}
